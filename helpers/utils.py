b459d72e836d548fa2b64ba6e477848baf17794b
mounaugusto <42648160+mounaugusto@users.noreply.github.com>
2025-04-04 12:23:20 +0100

    Update utils.py

--- helpers/utils.py
+++ helpers/utils.py
@@ -3,4 +3,4 @@ from __future__ import annotations
 
 def run_docker_compose(config_dict: dict, command: list[str]) -> list[str]:
 
-    return ['docker', 'compose'] + command
+    return ['docker-compose'] + command
